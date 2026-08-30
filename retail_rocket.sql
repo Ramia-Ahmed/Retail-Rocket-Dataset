fc3e4ae0f290d3661e7ee68f3e520de9f59edaa1
Ramia Ahmed <ramia_ahmed@outlook.com>
2026-08-30 20:20:05 +0600

    CAR calculating

--- retail_rocket.sql
+++ retail_rocket.sql
@@ -1,2 +1,39 @@
-CREATE VIEW category_tree AS
-SELECT * FROM 'Archive/category_tree.csv'
+CREATE OR REPLACE VIEW category_tree AS
+SELECT * FROM 'Archive/category_tree.csv';
+
+CREATE OR REPLACE VIEW events AS
+SELECT * FROM 'Archive/events.csv';
+
+CREATE OR REPLACE VIEW item_properties AS
+SELECT * FROM 'Archive/item_properties*.csv';
+
+SELECT * FROM category_tree;
+SELECT * FROM events;
+SELECT * FROM item_properties;
+
+SELECT event, COUNT(*) AS count
+FROM events
+GROUP BY event
+ORDER BY count;
+
+SELECT visitorid, count(*) AS event_count
+FROM events
+GROUP BY visitorid
+ORDER BY event_count DESC
+LIMIT 5;
+
+SELECT
+    visitorid,
+    event,
+    itemid,
+    to_timestamp(timestamp / 1000) AS event_time
+FROM events
+WHERE visitorid = 1150086
+ORDER BY timestamp;
+
+SELECT
+    visitorid,
+    event,
+    itemid,
+    event_time,
+    
